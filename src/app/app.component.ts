a0cf021512525367b79d67d6f0804e102e2d24dc
wendymal <wendymal@usc.edu>
2019-08-08 15:24:30 -0700

    typo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shoah-commons-harness';
-  dates: Array<string> = ['2018-10-01T11:10:47Z', 'August 1st, 2018', '20190103', 'im a cat'];
+  dates = ['2018-10-01T11:10:47Z', 'August 1st, 2018', '20190103', 'im a cat'];
 }
