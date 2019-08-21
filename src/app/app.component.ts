c537c0e77f2c5078332110e3109b9a070d361291
wendymal <wendymal@usc.edu>
2019-08-21 10:05:20 -0700

    adding mods, better display example of library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shoah-commons-harness';
-  dates = ['2018-10-01T11:10:47Z', 'August 1, 2018', '20190103', 'im a cat','2012-10-10 15:30:30.287'];
+  dates = ['2018-10-01T11:10:47Z', 'August 1, 2018', '20190103', 'im a cat','2012-10-10 15:30:30.287', 31556926, 604800, 50];
 }
